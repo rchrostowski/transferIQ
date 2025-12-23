631035399ba839633d630d718e6e4fa12e60523d
rchrostowski <144856089+rchrostowski@users.noreply.github.com>
2025-12-23 16:12:45 -0500

    Update scheme_fit.py

--- utils/scheme_fit.py
+++ utils/scheme_fit.py
@@ -1,1 +1,9 @@
+def compute_scheme_fit(player_position, team_offense):
+    if player_position == "QB" and team_offense in ["Spread", "Air Raid"]:
+        return 0.9
+    if player_position == "RB" and team_offense == "Power":
+        return 0.9
+    if player_position == "WR" and team_offense == "Air Raid":
+        return 0.95
+    return 0.7
 
