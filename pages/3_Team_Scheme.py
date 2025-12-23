223659784d7607384f51d7da9ec7e427d1a7d2e5
rchrostowski <144856089+rchrostowski@users.noreply.github.com>
2025-12-23 16:13:58 -0500

    Update 3_Team_Scheme.py

--- pages/3_Team_Scheme.py
+++ pages/3_Team_Scheme.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+teams = pd.read_csv("data/teams.csv")
+team = st.selectbox("Team", teams["team"])
+st.json(teams[teams["team"] == team].to_dict("records")[0])
 
